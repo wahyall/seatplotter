c42a5b97d04a15af51f482c1275ca167a80c9477
wahyall <wahyall278@gmail.com>
2026-04-07 15:22:45 +0700

    Fix: login suspenseg

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,18 @@
 "use client"
 
-import { useState, useTransition } from "react"
+import { Suspense, useState, useTransition } from "react"
 import { useRouter, useSearchParams } from "next/navigation"
 import { Armchair, Eye, EyeOff, Loader2, Lock, User } from "lucide-react"
 
 export default function LoginPage() {
+  return (
+    <Suspense>
+      <LoginForm />
+    </Suspense>
+  )
+}
+
+function LoginForm() {
   const router = useRouter()
   const searchParams = useSearchParams()
   const redirectTo = searchParams.get("redirect") || "/dashboard"
