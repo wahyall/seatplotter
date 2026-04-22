6b26daf725cd2e9158716e9494ff0546e09c1fd5
wahyall <wahyall278@gmail.com>
2026-04-22 13:30:56 +0700

    Fix: middleware

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,7 @@ export function middleware(request: NextRequest) {
   // Public paths that don't need authentication
   const publicPaths = [
     "/login",
+    "/banners",
     "/api/auth",
     "/booking",
     "/api/booking",
